675aeb86c423893db8beb47bc91041f7687e5fb5
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-08 16:07:07 +0300

    Chakra UI installed, navbar component and signin, signup pages created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Signin from './pages/Auth/Signin';
+import Signup from './pages/Auth/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+       <Navbar/> 
+        <div id='content'>
+          <Switch>
+              <Route path="/" exact component={Home}>
+                <Home/>
+              </Route>
+              <Route path="/signin" component={Signin}>
+                <Signin/>
+              </Route>
+              <Route path="/signup" component={Signup}>
+                <Signup/>
+              </Route>
+            </Switch>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
