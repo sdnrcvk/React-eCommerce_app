3cd82485365a95c23a5b5e4fdd56a8a015c9a359
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-10 11:38:03 +0300

    order modal added to basket page

--- src/api.js
+++ src/api.js
@@ -64,3 +64,4 @@ export const fetchProfile=async()=>{
 
     return data;
 }
+
--- src/pages/Basket/index.js
+++ src/pages/Basket/index.js
@@ -1,13 +1,37 @@
-import { Alert, Button, Image, Text, Box } from '@chakra-ui/react';
-import React from 'react'
+import { Alert, Button, Image, Text, Box, 
+Modal,
+ModalOverlay,
+ModalContent,
+ModalHeader,
+ModalFooter,
+ModalBody,
+ModalCloseButton, 
+useDisclosure,
+FormControl,
+FormLabel,
+Textarea
+} from '@chakra-ui/react';
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { useBasket } from '../../contexts/BasketContext'
 
 function Basket() {
-    const {items, removeFromBasket}=useBasket();
+    const {items, removeFromBasket, emptyBasket}=useBasket();
     const total=items.reduce((acc,obj)=>acc+obj.price,0);
+    const [address,setAddress]=useState("");
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
+    const initialRef = useRef(null)
+
+    const handleSubmitForm=async()=>{
+        const itemIds=items.map((item)=>item.id);
+
+        console.log("Sipariş verildi.");
+        emptyBasket();
+        onClose();
+    };
     console.log(items)
+
   return (
     <Box p="5">
         {items.length < 1 && (
@@ -40,6 +64,34 @@ function Basket() {
                     Toplam Tutar : {total} TL
                     </Text>
                 </Box>
+                <Button mt="2" size="sm" colorScheme="green" onClick={onOpen}>
+                    Sipariş Ver
+                </Button>
+                <Modal
+                    initialFocusRef={initialRef}
+                    isOpen={isOpen}
+                    onClose={onClose}
+                >
+                    <ModalOverlay />
+                    <ModalContent>
+                    <ModalHeader>Sipariş</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl>
+                        <FormLabel>Adres</FormLabel>
+                        <Textarea ref={initialRef} placeholder='Adres bilgisini giriniz' value={address}
+                        onChange={(e)=>setAddress(e.target.value)} />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={handleSubmitForm}>
+                        Kaydet
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                    </ModalContent>
+                </Modal>
             </>
             )
         }
